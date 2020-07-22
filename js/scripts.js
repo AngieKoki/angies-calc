1c3058e66037944fc151477a752dc0f754d6859c
Angie Koki <angie.dar66@gmail.com>
2020-07-22 17:36:32 +0300

    build a calculator program

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//BUSINESS LOGIC
+
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+//PREVIOUS USER LOGIC
+/*var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));*/
+
+
+//ADVANCED FRONT-END LOGIC
+$(document).ready(function() {
+    $('form#add').submit(function(event){
+        event.preventDefault();
+        var number1 = parseInt($('#add1').val());
+        var number2 = parseInt($('#add2').val());
+        var result = add(number1,number2)
+        $('#output').text(result);
+    });
+});
